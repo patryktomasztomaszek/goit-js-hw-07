8e3d30b8fc3cc021688742a91201dbebff297dab
Patryk Tomasz Tomaszek <patryk.tomasz.tomaszek@gmail.com>
2022-05-22 06:01:30 +0200

    Task 02
    In progress

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const imagesHTML = (imagesArray) =>
+  imagesArray
+    .map(
+      ({ original, preview, description }) =>
+        `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        `
+    )
+    .join("");
+
+gallery.insertAdjacentHTML("beforeend", imagesHTML(galleryItems));
+console.log(gallery);
+
+let lightbox = new SimpleLightbox(".gallery a",);
+lightbox.on("show.simplelightbox", function altCaptions () {
+    // derp
+    captions: true;
+    captionsData: "alt";
+})
